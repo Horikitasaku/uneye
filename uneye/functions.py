1455113e633f2cd0e15783ca168aeb7a50b2c301
Horikita_Saku <saku2696983188@gmail.com>
2023-09-04 14:38:03 +0800

    Update functions.py

--- uneye/functions.py
+++ uneye/functions.py
@@ -8,7 +8,7 @@ import os
 from skimage.measure import label
 from sklearn import metrics
 from scipy import io
-#from IPython import display
+# from IPython import display
 # Pytorch imports:
 import torch
 import torch.nn as nn
@@ -16,78 +16,77 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 
-
 ################################
 # Neural Network Architecture: #
 ################################
 class UNet(nn.Module):
-    def __init__(self,dim=2,ks=5,mp=5):
-        super(UNet,self).__init__()
+    def __init__(self, dim=2, ks=5, mp=5):
+        super(UNet, self).__init__()
         ''''
         dim: number of output features
         ks: kernel size of convolutional operations
         mp: kernel size of max pooling
         '''
-        Ch = [1,10,20,40,dim] # number of features
-        pd = int((ks-1)/2) # number of bins for border padding on each side
- 
+        Ch = [1, 10, 20, 40, dim]  # number of features
+        pd = int((ks - 1) / 2)  # number of bins for border padding on each side
+
         self.c0 = nn.Sequential(
-            nn.Conv2d(Ch[0],Ch[1],(ks,2),stride=1,padding=(pd,0)),
+            nn.Conv2d(Ch[0], Ch[1], (ks, 2), stride=1, padding=(pd, 0)),
             nn.ReLU(True),
-            nn.BatchNorm2d(Ch[1],affine=True),
+            nn.BatchNorm2d(Ch[1], affine=True),
         )
         self.c1 = nn.Sequential(
-            nn.Conv1d(Ch[1],Ch[2],ks,stride=1,padding=pd),
+            nn.Conv1d(Ch[1], Ch[2], ks, stride=1, padding=pd),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True),
+            nn.BatchNorm1d(Ch[2], affine=True),
         )
         self.p1 = nn.Sequential(
-            nn.MaxPool1d(kernel_size=mp,stride=mp),
+            nn.MaxPool1d(kernel_size=mp, stride=mp),
         )
         self.c2 = nn.Sequential(
-            nn.Conv1d(Ch[2],Ch[2],ks,stride=1,padding=pd),
+            nn.Conv1d(Ch[2], Ch[2], ks, stride=1, padding=pd),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True),
+            nn.BatchNorm1d(Ch[2], affine=True),
         )
         self.p2 = nn.Sequential(
-            nn.MaxPool1d(kernel_size=mp,stride=mp),
+            nn.MaxPool1d(kernel_size=mp, stride=mp),
         )
         self.c3 = nn.Sequential(  
-            nn.Conv1d(Ch[2],Ch[2],ks,stride=1,padding=pd),
+            nn.Conv1d(Ch[2], Ch[2], ks, stride=1, padding=pd),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True), 
+            nn.BatchNorm1d(Ch[2], affine=True), 
         )
         self.up1 = nn.Sequential(
-            nn.ConvTranspose1d(Ch[2],Ch[2],mp,stride=mp,padding=0),
+            nn.ConvTranspose1d(Ch[2], Ch[2], mp, stride=mp, padding=0),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True),
+            nn.BatchNorm1d(Ch[2], affine=True),
         )
         self.c4 = nn.Sequential(  
-            nn.Conv1d(Ch[3],Ch[2],ks,stride=1,padding=pd),
+            nn.Conv1d(Ch[3], Ch[2], ks, stride=1, padding=pd),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True),  
+            nn.BatchNorm1d(Ch[2], affine=True),  
         )
         self.up2 = nn.Sequential(
-            nn.ConvTranspose1d(Ch[2],Ch[2],mp,stride=mp,padding=0),
+            nn.ConvTranspose1d(Ch[2], Ch[2], mp, stride=mp, padding=0),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True),
+            nn.BatchNorm1d(Ch[2], affine=True),
         )
         self.c5 = nn.Sequential(
-            nn.Conv1d(Ch[3],Ch[2],ks,stride=1,padding=pd),
+            nn.Conv1d(Ch[3], Ch[2], ks, stride=1, padding=pd),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[2],affine=True),
+            nn.BatchNorm1d(Ch[2], affine=True),
         )
         self.c6 = nn.Sequential(
-            nn.Conv1d(Ch[2],Ch[1],ks,stride=1,padding=pd),
+            nn.Conv1d(Ch[2], Ch[1], ks, stride=1, padding=pd),
             nn.ReLU(True),
-            nn.BatchNorm1d(Ch[1],affine=True),
-        ) 
-        self.c7 = nn.Conv1d(Ch[1],Ch[4],1,stride=1,padding=0)     
-        
+            nn.BatchNorm1d(Ch[1], affine=True),
+        )
+        self.c7 = nn.Conv1d(Ch[1], Ch[4], 1, stride=1, padding=0)     
+
         self.sftmax = nn.Softmax(dim=1)
-        
-    def forward(self,input,outkeys):
-        
+
+    def forward(self, input, outkeys):
+
         out = {}
         out['in'] = input
         out['c0'] = self.c0(input).squeeze(3)
@@ -102,41 +101,39 @@ class UNet(nn.Module):
         out['c5'] = self.c5(torch.cat((out['c1'],out['up2']),1))
         out['c6'] = self.c6(out['c5'])
         out['out'] = self.sftmax(self.c7(out['c6']))
-        
+
         return [out[key] for key in outkeys]
 
-        
-        
-        
+
 ###############################
 ###### helper functions: ######
 ###############################
- 
+
 # Initialization of weights of convolutional layers
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         m.weight.data.normal_(0.0, 0.02)
 
-    
+
 # Multi-class loss
 class MCLoss(nn.Module):
     '''
     Torch cost function with foward method. 
     '''
     def __init__(self):
-        super(MCLoss,self).__init__()
-           
-    def forward(self,prediction,target):
-        
+        super(MCLoss, self).__init__()
+
+    def forward(self, prediction, target):
+
         epsilon = 1e-7
-        prediction = torch.clamp(prediction,min=epsilon)
-        prediction = torch.clamp(prediction,max=1-epsilon)  
-        E = - torch.mean( target * torch.log(prediction) )
+        prediction = torch.clamp(prediction, min=epsilon)
+        prediction = torch.clamp(prediction, max=1-epsilon)  
+        E = - torch.mean(target * torch.log(prediction))
 
         return E
-    
-    
+
+
 def lr_decay(optimizer, lr_decay=0.5):
     '''
     Decay learning rate by a factor of lr_decay
@@ -148,36 +145,37 @@ def lr_decay(optimizer, lr_decay=0.5):
     return optimizer
 
 
-def add_noise(X,Y,sd):
+def add_noise(X, Y, sd):
     """
     Add white noise to data for data augmentation
-    
+
     Parameters
     ----------
     X: array-like, horizontal eye trace
     Y: array-like, vertical eye trace
     sd: list of floats, shape=2, standard deviation of the white noise in a range [min,max]
-    
+
     Output
     ------
     Xnoise: array-like, horizontal eye trace + white noise
     Ynoise: array-like, vertical eye trace + white noise
-    
+
     """
     t = X.shape[1]
     N = X.shape[0]
     np.random.seed(20)
-    
-    trial_noise_level = np.tile(np.random.rand(N,1) * (sd[1]-sd[0]) + sd[0], (1,t));
-    noise4X = np.multiply(np.random.randn(N,t),trial_noise_level)
-    noise4Y = np.multiply(np.random.randn(N,t),trial_noise_level)
-    
-    return X+noise4X , Y+noise4Y
 
-def merge_saccades(Prediction,samp_freq,min_sacc_dist=10):
+    trial_noise_level = np.tile(np.random.rand(N, 1) * (sd[1]-sd[0]) + sd[0], (1, t))
+    noise4X = np.multiply(np.random.randn(N, t), trial_noise_level)
+    noise4Y = np.multiply(np.random.randn(N, t), trial_noise_level)
+
+    return X + noise4X, Y + noise4Y
+
+
+def merge_saccades(Prediction, samp_freq, min_sacc_dist=10):
     '''
     Merge saccades that are too close in time
-    
+
     Parameters
     ----------
     Prediction: array-like, shape={(n_timepoints),(n_samples,n_timepoints)}, binary saccade prediction
@@ -188,95 +186,95 @@ def merge_saccades(Prediction,samp_freq,min_sacc_dist=10):
     ------
     Prediction_new = array-like, shape={(n_timepoints),(n_samples,n_timepoints)}, binary saccade prediction
     '''
-    Prediction2 = (Prediction.copy()==0).astype(int)
+    Prediction2 = (Prediction.copy() == 0).astype(int)
     Prediction_new = Prediction.copy()
-    
-    if len(Prediction.shape)<2: # case where network output is a vector
+
+    if len(Prediction.shape) < 2:  # case where network output is a vector
         l = label(Prediction2)
-        first_label = 1 + int(l[0]==1)
-        last_label = np.max(l) - int(l[-1]==np.max(l))
-        for i in range(first_label,last_label+1):
-            if np.sum(l==i)<int(min_sacc_dist*(samp_freq/1000)):
-                Prediction_new[l==i] = 1
-                    
+        first_label = 1 + int(l[0] == 1)
+        last_label = np.max(l) - int(l[-1] == np.max(l))
+        for i in range(first_label, last_label+1):
+            if np.sum(l == i) < int(min_sacc_dist*(samp_freq/1000)):
+                Prediction_new[l == i] = 1
+
     else:  # case where network output is a matrix
         for n in range(Prediction.shape[0]):
-            l = label(Prediction2[n,:])
-            first_label = 1 + int(l[0]==1)
-            last_label = np.max(l) - int(l[-1]==np.max(l))
-            for i in range(first_label,last_label+1):
-                if np.sum(l==i)<int(min_sacc_dist*(samp_freq/1000)):
-                    Prediction_new[n,l==i] = 1
-                    
-    return Prediction_new        
-            
-    
-def binary_prediction(output,samp_freq,p=0.5,min_sacc_dist=1,min_sacc_dur=1): 
+            l = label(Prediction2[n, :])
+            first_label = 1 + int(l[0] == 1)
+            last_label = np.max(l) - int(l[-1] == np.max(l))
+            for i in range(first_label, last_label+1):
+                if np.sum(l == i) < int(min_sacc_dist*(samp_freq/1000)):
+                    Prediction_new[n, l == i] = 1
+
+    return Prediction_new
+
+
+def binary_prediction(output, samp_freq, p=0.5, min_sacc_dist=1, min_sacc_dur=1): 
     '''
     Predict saccades from network probability output.
     Apply threshold on softmax and delete saccade of length < min_sacc_dur.
-    
+
     Parameters
     ----------
     output: array-like, shape={(n_timepoints), (n_samples,n_timepoints)}, network softmax output
     samp_freq: int, sampling frequency in Hz
     p: float, threshold for saccade class, default=0.5
     min_sacc_dur: int, minimum saccade duration for removal of small saccades, default=1 (==no filtering)
-    
+
     Output
     ------
     S_predic: array-like, shape={(n_timepoints), (n_samples,n_timepoints)}, fixation(=0) and saccade(=1) prediction for each time_bin in the network output
-    
+
     '''
-    S_predic = (output>p).astype(int)
-    if min_sacc_dist!=1:
+    S_predic = (output > p).astype(int)
+    if min_sacc_dist != 1:
         # merge saccades with distance < min_sacc_dist
-        S_predic = merge_saccades(S_predic,samp_freq,min_sacc_dist)
-    
+        S_predic = merge_saccades(S_predic, samp_freq, min_sacc_dist)
+
     # delete small saccades                
-    if min_sacc_dur>1:
-        if len(output.shape)<2:
+    if min_sacc_dur > 1:
+        if len(output.shape) < 2:
             # case where network output is a vector
             l = label(S_predic)
-            for j in range(1,np.max(l)+1):
-                s = np.sum(l==j)
-                if s<min_sacc_dur:
-                    S_predic[l==j] = 0
+            for j in range(1, np.max(l) + 1):
+                s = np.sum(l == j)
+                if s < min_sacc_dur:
+                    S_predic[l == j] = 0
         else:
             # case where network output is a matrix; assume: first dimension: number of samples
             for n in range(output.shape[0]):
-                l = label(S_predic[n,:])
-                for j in range(1,np.max(l)+1):
-                    s = np.sum(l==j)
-                    if s<min_sacc_dur:
-                        S_predic[n,l==j] = 0                        
-    return S_predic  
+                l = label(S_predic[n, :])
+                for j in range(1, np.max(l)+1):
+                    s = np.sum(l == j)
+                    if s < min_sacc_dur:
+                        S_predic[n, l == j] = 0              
+    return S_predic
 
 
-def cluster_belonging (logical_array,index):
+def cluster_belonging(logical_array, index):
     '''
     return the cluster of 'True' values that contain a time point
     logical_array: 1D array of 'True' and 'False'
     index value between 0 and length of logical_array
     '''
-    
+
     labels = label(logical_array)
-    
-    cluster=(np.arange(0,len(logical_array))*0).astype(bool)
-    cluster[labels==labels[index]]=True              
+
+    cluster = (np.arange(0, len(logical_array))*0).astype(bool)
+    cluster[labels == labels[index]] = True              
     return cluster
 
 
-def accuracy (predicted,truth):
+def accuracy(predicted, truth):
     '''
     Computes accuracy of prediction compared to groundtruth (in terms of true and false positive and false negative detections)
     Implemented for saccades only (no pso)
-    
+
     Parameters
     ----------
     predicted: array-like, shape={(n_timepoints),(n_samples,n_timepoints)}, fixation(=0) and saccade(=1) prediction
     truth: array-like, shape={(n_timepoints),(n_samples,n_timepoints)}, fixation(=0) and saccade(=1) labels
-    
+
     Output
     ------
     true_pos: float, number of correctly detected saccades
@@ -284,10 +282,10 @@ def accuracy (predicted,truth):
     false_neg: float, number of missed saccades
     on_distance: array-like, time-bin difference between true and predicted saccade onset for all true positives
     off_distance: array-like, time-bin difference between true and predicted saccade offset for all true positives
-    
+
     '''
-    
-    if len(truth.shape)>1:
+
+    if len(truth.shape) > 1:
         # assume first dimension is number of samples
         batchsize = predicted.shape[0]
     else:
@@ -296,27 +294,27 @@ def accuracy (predicted,truth):
         # enforce two dimensions
         truth = np.atleast_2d(truth)
         predicted = np.atleast_2d(predicted)
-        
+
     truth_copy = truth.copy()
     truth = truth.astype(int)
     pred_copy = predicted.copy()
-    
+
     # Velocity
-    Diff_p = np.diff(predicted,axis=1)
-    Diff_t = np.diff(truth,axis=1)
-    
+    Diff_p = np.diff(predicted, axis=1)
+    Diff_t = np.diff(truth, axis=1)
+
     # count variables:
     true_pos = 0
     false_neg = 0
     false_pos = 0
     on_distance = []
     off_distance = []
-    
+
     # loop trough samples:
     for i in range(batchsize):
-        trace_t = Diff_t[i,:]
-        trace_p = Diff_p[i,:]
-        
+        trace_t = Diff_t[i, :]
+        trace_p = Diff_p[i, :]
+
         # find start and end of saccades
         # labels from prediction
         start_p = np.argwhere(trace_p==1)+1 # add one as the diff is shifted one bin early
@@ -324,19 +322,19 @@ def accuracy (predicted,truth):
         # labels from ground truth
         start_t = np.argwhere(trace_t==1)+1 # add one as the diff is shifted one bin early
         end_t = np.argwhere(trace_t==-1)+1 # add one as python indexing doesn't include last index in range
-        
-        
+
+
         # exclude border saccades in ground truth data:
         if (truth[i,0]==1):
             end_t = end_t[1:]
         if (truth[i,-1]==1):
             start_t = start_t[:-1]
-    
+
         if len(start_t)!=0: # if there is at least one saccade
-            
+
             # if all conditions met, loop through each true saccade
             for j in range(len(start_t)):
-                
+
                 content_of_pred = pred_copy[i,int(start_t[j]):int(end_t[j])] #content of prediction during true saccade duration 
                 
                 if content_of_pred!=[]:
